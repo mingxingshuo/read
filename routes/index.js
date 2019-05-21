4232369e676ebfe4a830260a093b40070a388961
Administrator <lionel@buzzy.cc>
2019-05-21 14:27:22 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -130,10 +130,20 @@ router.get('/amount', async (ctx, next) => {
 	ctx.body = reads
 })
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
+router.get('/data', async (ctx, next) => {
+  let trades = await redis_client.smembers('shua_trans_list')
+  let arr=[]
+  for (var i = 0; i < trades.length; i++) {
+  	var trade = trades[i];
+  	let uv = await redis_client.pfcount('shua_read_tradeNo_uv_'+trade)
+  	let pv = await redis_client.get('shua_read_tradeNo_'+read.tradeNo)
+  	arr.push({
+  		tradeNo :trade,
+  		uv : uv,
+  		pv : pv
+  	})
   }
+  ctx.body = arr
 })
 
 module.exports = router
---
+++ script/data.js
@@ -0,0 +1,18 @@
+const asyncRedis = require("async-redis");
+const redis_client = asyncRedis.createClient();
+
+async function getdata() {
+  let trades = await redis_client.smembers('shua_trans_list')
+  let arr=[]
+  for (var i = 0; i < trades.length; i++) {
+    var trade = trades[i];
+    let uv = await redis_client.pfcount('shua_read_tradeNo_uv_'+trade)
+    let pv = await redis_client.get('shua_read_tradeNo_'+read.tradeNo)
+    arr.push({
+      tradeNo :trade,
+      uv : uv,
+      pv : pv
+    })
+  }
+  console.log(arr)
+}
