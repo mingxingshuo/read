9a702ef0526c90d0201b128711a16daa804053e2
Administrator <lionel@buzzy.cc>
2019-05-30 15:05:57 +0800

    1

--- script/data.js
+++ script/data.js
@@ -219,6 +219,8 @@ async function get_slef_zong_data() {
 
 //get_self_data()
 
-get_slef_zong_data()
+//get_slef_zong_data()
 
-//get_doumeng_zong_data('20190527')
+get_doumeng_zong_data('20190527')
+get_doumeng_zong_data('20190528')
+get_doumeng_zong_data('20190529')
