96aab4ce5fd76b4e46a36d56a1559ff9329807e2
Administrator <lionel@buzzy.cc>
2019-05-30 17:51:00 +0800

    1

--- script/data.js
+++ script/data.js
@@ -215,11 +215,40 @@ async function get_slef_zong_data() {
   })
 }
 
+
+async function get_wowo_zong_data() {
+  let total = 0;
+  let arr=[]
+  let zong_trads = await redis_client.smembers('wowo_shua_trans_list')
+  for (var i = 0; i < zong_trads.length; i++) {
+    var trade = zong_trads[i];
+    //console.log(trade)
+    let uv = await redis_client.pfcount('self_shua_read_tradeNo_uv_'+trade)
+    if(trade.indexOf('20190530')!=-1){
+      if(uv!=0){
+        total += uv;
+        arr.push({
+          tradeNo :trade,
+          uv : uv
+        })
+      }
+    }
+  }
+  console.log('--------自己--------')
+  console.log({
+    total : total,
+    //arr : arr
+  })
+}
+
+
+get_wowo_zong_data()
+
 //get_doumeng_data()
 
 //get_self_data()
 
-get_slef_zong_data()
+//get_slef_zong_data()
 
 //get_doumeng_zong_data('20190527')
 //get_doumeng_zong_data('20190528')
