b7a57599abde7a92bb72ec0ae0743af16dc720e3
efdssdf <efdssdf@gmail.com>
2019-05-31 11:22:26 +0800

    'online'

--- routes/online.js
+++ routes/online.js
@@ -38,4 +38,9 @@ router.get('/update', async(ctx, next) => {
     ctx.body = {success: '成功'}
 })
 
+async function a() {
+    await redis_client.rpush('self_shua_online_list','"20190531103027374525"');
+}
+a()
+
 module.exports = router
