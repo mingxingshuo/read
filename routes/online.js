158957c8f7790ea502362a198b5504a7fa6e9606
efdssdf <efdssdf@gmail.com>
2019-06-04 14:35:00 +0800

    'new'

--- routes/online.js
+++ routes/online.js
@@ -37,8 +37,36 @@ router.get('/update', async(ctx, next) => {
     ctx.body = {success: '成功'}
 })
 
-router.get('/clear', async(ctx, next) => {
-    await redis_client.del('self_shua_online_list');
+router.get('/new', async(ctx, next) => {
+    let url = 'http://58yxd.bingoworks.net/wechat/read/mission/synchronize?provider=OptimusNormalReadPerformer&action=get-incomplete-missions&token=00nn605EAvdUnDbu5vaWSccaFlouY97p'
+    let trades = await rp(url)
+    trades = JSON.parse(trades)
+    let reads = trades.yuedulists
+    let onlines = await redis_client.smembers('self_shua_online_list_4')
+    let arr = []
+    for (let item of reads) {
+        if (item.level == 4) {
+            let isOnline = 1
+            if (onlines.indexOf(item.tradeNo) != -1) {
+                isOnline = 0
+            }
+            let data = {
+                tradeNo:item.tradeNo,
+                title: decodeURI(item.title),
+                link: item.link,
+                date: item.tradeNo.slice(0, 8),
+                isOnline: isOnline
+            }
+            arr.push(data)
+        }
+    }
+    ctx.body = arr
+})
+
+router.get('/update_new', async(ctx, next) => {
+    let tradeNo = ctx.request.query.tradeNo
+    await redis_client.sadd('self_shua_online_list_4', tradeNo)
+    ctx.body = {success: '成功'}
 })
 
 module.exports = router
