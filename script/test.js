b104bee28d60aef2fe7f801c25aa65821f5d36e1
efdssdf <efdssdf@gmail.com>
2019-06-06 11:54:05 +0800

    'clear'

--- script/test.js
+++ script/test.js
@@ -2,7 +2,7 @@ const asyncRedis = require("async-redis");
 const redis_client = asyncRedis.createClient();
 
 async function test() {
-    await redis_client.del('self_shua_online_list', '20190605233630624900')
+    await redis_client.del('self_shua_online_list',0, '20190605233630624900')
     let test = await redis_client.smembers('self_shua_online_list')
     console.log(test,'---------------test')
     // console.log(test,'---------------test')
