3674cd9b1e2a6f26037079a1adcff384912dc66c
efdssdf <efdssdf@gmail.com>
2019-06-06 15:18:14 +0800

    'clear'

--- script/test.js
+++ script/test.js
@@ -1,12 +1,78 @@
 const asyncRedis = require("async-redis");
 const redis_client = asyncRedis.createClient();
 
-async function test() {
-    let test = await redis_client.smembers('self_shua_online_list')
-    console.log(test,'---------------test')
-    await redis_client.srem('self_shua_online_list', '20190605233630624900')
-    let test1 = await redis_client.smembers('self_shua_online_list')
-    console.log(test1,'---------------test')
-    // console.log(test,'---------------test')
+async function clearSelf(time) {
+    let self = await redis_client.smembers('self_shua_trans_list')
+    let arr = []
+    for (let item of self) {
+        let tradeTime = item.slice(0, 8)
+        if (parseInt(time) - parseInt(tradeTime) >= 4) {
+            let a = await redis_client.get('self_shua_read_tradeNo_' + item)
+            arr.push(a)
+        }
+    }
+    console.log('---------' + arr.length + '------------self')
+    return
 }
-test()
+
+async function clearChao(time) {
+    let chao = await redis_client.smembers('chao_shua_trans_list')
+    let arr = []
+    for (let item of chao) {
+        let tradeTime = item.slice(0, 8)
+        if (parseInt(time) - parseInt(tradeTime) >= 4) {
+            let a = await redis_client.get('self_shua_read_tradeNo_' + item)
+            arr.push(a)
+        }
+    }
+    console.log('---------' + arr.length + '------------chao')
+    return
+}
+
+async function clearDoumeng(time) {
+    let doumeng = await redis_client.smembers('new_shua_trans_list')
+    let arr = []
+    for (let item of doumeng) {
+        let tradeTime = item.slice(0, 8)
+        if (parseInt(time) - parseInt(tradeTime) >= 4) {
+            let a = await redis_client.get('self_shua_read_tradeNo_' + item)
+            arr.push(a)
+        }
+    }
+    console.log('---------' + arr.length + '------------doumeng')
+    return
+}
+
+async function clearIndex(time) {
+    let index = await redis_client.smembers('shua_trans_list')
+    let arr = []
+    for (let item of index) {
+        let tradeTime = item.slice(0, 8)
+        if (parseInt(time) - parseInt(tradeTime) >= 4) {
+            let a = await redis_client.get('self_shua_read_tradeNo_' + item)
+            arr.push(a)
+        }
+    }
+    console.log('---------' + arr.length + '------------index')
+    return
+}
+
+async function clearWowo(time) {
+    let wowo = await redis_client.smembers('wowo_shua_trans_list')
+    let arr = []
+    for (let item of wowo) {
+        let tradeTime = item.slice(0, 8)
+        if (parseInt(time) - parseInt(tradeTime) >= 4) {
+            let a = await redis_client.get('self_shua_read_tradeNo_' + item)
+            arr.push(a)
+        }
+    }
+    console.log('---------' + arr.length + '------------wowo')
+    return
+}
+
+clearSelf()
+clearChao()
+clearDoumeng()
+clearIndex()
+clearWowo()
